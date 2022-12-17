45b02b95222e0437993202c4f2a11be3c77359d6
Matheus Melo <matheuscabal821@gmail.com>
2022-12-17 00:21:55 -0300

    📦 NEW: New component HomePage added but not 100%

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,57 @@
+import styled from "styled-components";
+import { IoExitOutline } from "react-icons/io5";
 
 export default function HomePage (){
-  return <h1>Home Page</h1>
+  return (
+    <BackgroundHomePage>
+      <Header>
+        <h1>Ola, Fulano</h1>
+        <IoExitOutline className="iconExit"/>
+      </Header>
+      <Dashboard></Dashboard>
+      <Footer></Footer>
+    </BackgroundHomePage>
+  );
 }
+
+const BackgroundHomePage = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 10px;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+
+  h1{
+    font-weight: 800;
+    font-size: 1.5em;
+  }
+
+  .iconExit {
+    font-size: 1.8em;
+    cursor: pointer;
+    &:hover{
+      color: white;
+    }
+  }
+
+`;
+
+const Dashboard = styled.div`
+  background-color: white;
+  width: 100%;
+  height: 300px;
+  border-radius: 4px;
+`;
+
+const Footer = styled.div`
+
+`;
