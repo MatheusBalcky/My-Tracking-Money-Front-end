18044f1892587c5e137a649877788b48c185e9a7
Matheus Melo <matheuscabal821@gmail.com>
2022-12-19 22:19:16 -0300

    📦 NEW-COMPONENT: Component wich render months added

---
+++ src/components/homeComponents/interfacesAndDatas.tsx
@@ -0,0 +1,20 @@
+
+export interface BackgroundPropsI {
+  monthsIsFlex: string;
+}
+export interface MonthOptionsI {
+  monthsIsFlex: string;
+}
+export const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Novembro',
+  'Dezembro',
+];
