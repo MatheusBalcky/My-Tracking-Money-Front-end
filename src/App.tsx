774eca6ddb4665f12f2e5fd9571cb3425e1edc1b
Matheus Melo <matheuscabal821@gmail.com>
2022-12-22 23:11:29 -0300

    📦 NEW: New routes added

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import ApresentationPage from "./pages/ApresentationPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import NewTransactionPage from "./pages/NewTransactionPage";
+import MyWalletPage from "./pages/MyWalletPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"*" || "/apresentation"} element={<ApresentationPage />} />
+        <Route path={'*' || '/apresentation'} element={<ApresentationPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/new-transaction" element={<NewTransactionPage />} />
+        <Route path="/my-wallet" element={<MyWalletPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
   );
