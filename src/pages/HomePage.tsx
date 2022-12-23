cdcee1242bb0b5e64592e49b30a6e82e76e4e4d2
Matheus Melo <matheuscabal821@gmail.com>
2022-12-22 21:34:45 -0300

    📦 NEW: Transactions and Footer layouts added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,33 @@
 import styled from 'styled-components';
-import { IoExitOutline } from 'react-icons/io5';
+import {
+  IoExitOutline,
+  IoWalletOutline,
+  IoSettingsOutline,
+} from 'react-icons/io5';
+import { HiArrowsRightLeft } from 'react-icons/hi2';
 import SelectMonth from '../components/homeComponents/selectMonth';
-import { months } from '../components/homeComponents/interfacesAndDatas';
+import {
+  months,
+  transactionsExample,
+} from '../components/homeComponents/interfacesAndDatas';
 import MoneyBalance from '../components/homeComponents/moneyBalance';
+import { useNavigate } from 'react-router-dom';
+import formatValueToBr from '../utils/formatValues';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function logout() {
+    alert('Deslogando da aplicação');
+    navigate('/apresentation');
+  }
+
   return (
     <BackgroundHomePage>
       <Header>
         <div className="topHeader">
           <h1> &gt; Ola, Fulano</h1>
-          <IoExitOutline className="iconExit" />
+          <IoExitOutline onClick={logout} className="iconExit" />
         </div>
 
         <InfoHeader>
@@ -19,8 +36,35 @@ export default function HomePage() {
           <MoneyBalance />
         </InfoHeader>
       </Header>
-      <Dashboard></Dashboard>
-      <Footer></Footer>
+
+      <BodyTransactions>
+        {transactionsExample.map((item) => {
+          return (
+            <TransactionComponent key={item.id} about={item.type}>
+              <div className="dateAndName">
+                <p className="date">{item.date}</p>
+                <p>{item.title}</p>
+              </div>
+              <p className="value">{formatValueToBr(item.value)}</p>
+            </TransactionComponent>
+          );
+        })}
+      </BodyTransactions>
+
+      <Footer>
+        <IconFooter>
+          <HiArrowsRightLeft className="iconMenu" />
+          <p>Transações</p>
+        </IconFooter>
+        <IconFooter>
+          <IoWalletOutline className="iconMenu" />
+          <p>Carteira</p>
+        </IconFooter>
+        <IconFooter>
+          <IoSettingsOutline className="iconMenu" />
+          <p>Configurações</p>
+        </IconFooter>
+      </Footer>
     </BackgroundHomePage>
   );
 }
@@ -29,13 +73,15 @@ const BackgroundHomePage = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  padding: 10px;
+  justify-content: space-between;
+  height: 99.9vh;
   align-items: center;
   gap: 10px;
 `;
 
 const Header = styled.div`
+  box-sizing: border-box;
+  padding: 10px;
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -79,11 +125,56 @@ const InfoHeader = styled.div`
   }
 `;
 
-const Dashboard = styled.div`
+const BodyTransactions = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
   background-color: white;
+  padding: 20px;
   width: 100%;
-  height: 300px;
-  border-radius: 4px;
+  height: 100%; //! não mexe agora apenas qnd tiver footer pronto
 `;
 
-const Footer = styled.div``;
+const TransactionComponent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  .dateAndName {
+    display: flex;
+    gap: 18px;
+  }
+  .date {
+    color: lightgray;
+  }
+  .value {
+    color: ${(props) => (props.about === 'out' ? 'red' : '#03AC00')};
+    font-weight: 500;
+  }
+`;
+
+const Footer = styled.nav`
+  width: 100%;
+  background-color: black;
+  box-sizing: border-box;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+const IconFooter = styled.div`
+  color: white;
+
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .iconMenu {
+    font-size: 1.5em;
+  }
+  p {
+    font-size: 0.7em;
+  }
+`;
