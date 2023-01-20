fbc7d19e7ae5406eb57aa458203bfbb632dd9582
Matheus Melo <matheuscabal821@gmail.com>
2023-01-19 23:57:41 -0300

    📦 NEW: React Query Lib added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './App'
 import './styles/index.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppRoutes />
+    <QueryClientProvider client={queryClient} >
+      <AppRoutes />
+    </QueryClientProvider>
   </React.StrictMode>
-)
+);
