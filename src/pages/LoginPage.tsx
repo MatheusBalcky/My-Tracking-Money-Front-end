7952a9d966c93cf92f486fd2a46481bd8ad14186
Matheus Melo <matheuscabal821@gmail.com>
2023-01-20 00:01:08 -0300

    📦 NEW: API ADDED TO LOGIN PAGE

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,31 +1,64 @@
-import styled from "styled-components"
-import { Link, useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+import { signIn } from './api/authApi';
+import { useMutation } from 'react-query';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z, string } from 'zod';
+import { useState } from 'react';
 
-export default function LoginPage(){
+const loginSchema = z.object({
+  email: string().email({
+    message: 'E-mail inválido!',
+  }),
+  password: string(),
+});
+
+type LoginInputs = z.infer<typeof loginSchema>;
+
+export default function LoginPage() {
+  // prettier-ignore
+  const {register, handleSubmit, formState: { errors }} = useForm<LoginInputs>({ resolver: zodResolver(loginSchema) });
   const navigate = useNavigate();
+  const [invalidDataMessage, setInvalidDataMessage] = useState('');
 
-  const login = (e: React.FormEvent ) =>{
-    e.preventDefault();
-    navigate('/home')
-    console.log('Logando no sistema');
-  }
+  const mutation = useMutation((data: LoginInputs) => signIn(data), {
+    onSuccess: (data) => {
+      // ! console.log(data); SALVAR NUM CONTEXT, ETC... PROCURAR A LIB
+      navigate('/home');
+    },
+    onError: (error) => {
+      setInvalidDataMessage('Senha ou e-mail inválidos!');
+    },
+    onMutate: () => {
+      setInvalidDataMessage('');
+    },
+  });
+
+  const onSubmit = handleSubmit((data: LoginInputs) => mutation.mutate(data));
 
   return (
     <BackgroundLoginPage>
       <h1>My Money Tracking</h1>
 
-      <FormLoginPage onSubmit={(e) => login(e)}>
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button>Entrar</button>
+      <FormLoginPage onSubmit={onSubmit}>
+        <input {...register('email')} type="email" placeholder="E-mail" required />
+        <p>{errors.email?.message}</p>
+
+        <input {...register('password', { required: true, maxLength: 26 })} type="password" placeholder="Senha" />
+        <p>{invalidDataMessage}</p>
+
+        <button type="submit">Entrar</button>
       </FormLoginPage>
 
-      <Link to={"/register"}>
+      <Link to={'/register'}>
         <span>Primeira vez? Cadastre-se!</span>
       </Link>
     </BackgroundLoginPage>
   );
 }
+
+// & CSS STYLED COMPONENTS
 
 export const BackgroundLoginPage = styled.div`
   display: flex;
@@ -36,7 +69,7 @@ export const BackgroundLoginPage = styled.div`
   gap: 18px;
 
   h1 {
-    font-family: "Saira Stencil One", cursive;
+    font-family: 'Saira Stencil One', cursive;
     font-size: 1.5em;
   }
 
@@ -58,10 +91,9 @@ export const FormLoginPage = styled.form`
   flex-direction: column;
   align-items: center;
   gap: 8px;
-
   input,
   button {
-    font-family: "Raleway";
+    font-family: 'Raleway';
     border: none;
     border-radius: 4px;
     font-size: 1em;
@@ -77,5 +109,14 @@ export const FormLoginPage = styled.form`
     &:hover {
       filter: opacity(70%);
     }
+  }
+
+  p {
+    font-size: 0.6em;
+    color: red;
+    font-weight: bold;
+    text-align: center;
+    width: 75%;
+    box-sizing: border-box;
   }
 `;
